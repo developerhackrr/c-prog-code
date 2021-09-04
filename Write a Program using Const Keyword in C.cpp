30d7ea374a21a5a36c03e72690dd999a77b3e64e
Rahul hacker 420 <87141000+developerhackrr@users.noreply.github.com>
2021-09-04 09:42:32 +0530

    Add files via upload

---
+++ Write a Program using Const Keyword in C.cpp
@@ -0,0 +1,19 @@
+//Write a Program using Const Keyword in C//
+
+#include <stdio.h>
+int main()
+{
+const int a =10; //integer constant
+const float b =12.3f; // float constant
+const char c ='X'; // character constant
+const char str[] ="Hello, world!"; // string constant
+
+// printing the values
+printf("a = %d\n", a);
+printf("b = %f\n", b);
+printf("c = %c\n", c);
+printf("str = %s\n", str);
+
+return 0;
+}
+
