b98f0af9fa1e1d21d2f90ebc0c5f2ec8c59d0876
Rahul hacker 420 <87141000+developerhackrr@users.noreply.github.com>
2021-09-04 10:04:11 +0530

    Add files via upload

---
+++ convert integer datatype to double datatype.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+main() {
+
+   int sum = 17, count = 5;
+   double mean;
+
+   mean = (double) sum / count;
+   printf("Value of mean : %f\n", mean );
+}
+
+#include<stdio.h>
+
+main(){
+
+inti=17;
+char c ='c';/* ascii value is 99 */
+int sum;
+
+sum=i+ c;
+printf("Value of sum : %d\n", sum );
+}
+
